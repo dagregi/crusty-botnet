d72a67f60e2921efda83218f12542a3eca4b3a51
dagregi <120724828+dagregi@users.noreply.github.com>
2023-11-18 15:00:05 +0300

    basic tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::io;
+use std::net::TcpListener;
 
-use tokio::net::TcpListener;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:42069").expect("Failed to start server");
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    println!("Sup bitch");
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-    match listener.accept().await {
-        Ok((_socket, addr)) => println!("connected to {:?}", addr),
-        Err(e) => eprintln!("error {:?}", e),
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                let addr = stream.peer_addr().unwrap().to_string();
+                println!("Connected to :{}", addr);
+            }
+            Err(e) => {
+                eprintln!("Failed to accept connection: {}", e);
+            }
+        }
     }
-
-    Ok(())
 }
